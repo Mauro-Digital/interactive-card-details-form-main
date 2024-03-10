bb03f8c8477646a818e40578de20c7405fe64288
Mauro-Digital <141379319+Mauro-Digital@users.noreply.github.com>
2024-03-10 12:27:10 -0500

    Add files via upload

---
+++ sass/main.js
@@ -0,0 +1,22 @@
+// CARDHOLDER NAME
+// let nameCard = document.querySelector('.card__details-name');
+// let nameInput = document.querySelector('#cardholder');
+// let nameErrorDiv = document.querySelector('.form__cardholder-error error');
+
+// // Ingreso dinamico del nombre
+// nameInput.addEventListener('input', ()=>{
+//     console.log('nameInput');
+// });
+
+// CARDHOLDER NAME
+let nameCard = document.querySelector('.card__details-name');
+let nameInput = document.querySelector('#cardholder');
+let nameErrorDiv = document.querySelector('.form__cardholder-error error');
+
+// Ingreso dinamico del nombre
+nameInput.addEventListener('input', ()=>{
+    console.log('Ingreso de nombre');   
+});
+
+
+
